805d062fc2257bb13bf9a121a30279f24cdc1a39
Denis Lavrov <>
2025-12-22 14:44:35 +0700

    check detekt error

--- app/src/main/java/com/test/app/main/presentation/MainViewModel.kt
+++ app/src/main/java/com/test/app/main/presentation/MainViewModel.kt
@@ -12,6 +12,7 @@ import dagger.assisted.AssistedInject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.launch
+import java.time.LocalDateTime
 
 class MainViewModel @AssistedInject constructor(
     private val mainRepository: IMainRepository,
@@ -27,6 +28,7 @@ class MainViewModel @AssistedInject constructor(
     interface Factory : ViewModelAssistedFactory<MainViewModel>
 
     init {
+        val a = LocalDateTime.now()
         viewModelScope.launch {
             mainRepository.isAuthorized.collect {
                 dispatch(MainAction.Update(it))
